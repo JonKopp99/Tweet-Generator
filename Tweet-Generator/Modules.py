a15f350f41b484844cef2ef4ed71c7678fb3a461
JonKopp99 <jonkopp99@gmail.com>
2019-02-03 22:34:28 -0800

    Finished frequency method

--- Tweet-Generator/Modules.py
+++ Tweet-Generator/Modules.py
@@ -55,27 +55,49 @@ def tuplegram(theWords):
 
 #A unique_words() function that takes a histogram argument and returns the total count of unique words
 #in the histogram. For example, when given the histogram for The Adventures of Sherlock Holmes, it returns the integer 8475.
-def unique_words(histogram):
-    count = 0
-    for word in histogram:
-        count += 1
-    return count
+def unique_words(theWords):
+    ctr = 0
+    currentIndex = 0 #Keeps track of location in array
+    for word in theWords:
+        wordIndex = currentIndex + 1 #tracks location of what word we are comparing
+        ctr += 1
+        while(wordIndex < len(theWords)): #Loop through to compare
+            if(theWords[wordIndex] == word):
+                theWords.pop(wordIndex) #Remove duplicates from arr (no need to keep)
+            wordIndex += 1
+        currentIndex += 1
+    return ctr
 
 #A frequency() function that takes a word and histogram argument and returns the number of times that word appears in a text.
 #For example, when given the word "mystery" and the Holmes histogram, it will return the integer 20.
-def frequency():
-    pass
+def frequency(word, theWords):
+    ctr = 0
+    for i in theWords:
+        if(i == word):
+            ctr += 1
+
+    return ctr
 
 
 if __name__ == '__main__':
     tempStringArr = ["one", "fish", "two", "fish","red", "fish","blue","fish"]
-    dictionaryValue = dict_togram(tempStringArr)
-    print(dictionaryValue)
-    countOfUniqueWords = unique_words(dictionaryValue)
-    print(countOfUniqueWords)
 
+    #dictionary test
+    #dictionaryValue = dict_togram(tempStringArr)
+    #print(dictionaryValue)
+
+    #unique words test
+    #countOfUniqueWords = unique_words(tempStringArr)
+    #print(countOfUniqueWords)
+
+    #frequency test
+    freq = frequency("fish", tempStringArr)
+    print(freq)
+
+    #matrix list test
     #listValues = listtogram(tempStringArr)
     #print(listValues)
 
+    #tuple tetst
     #tupleValues = tuplegram(tempStringArr)
     #print(tupleValues)
