d2532300ea42431604728fdb1f36b9eca3ac0395
JonKopp99 <jonkopp99@gmail.com>
2019-02-27 14:27:11 -0800

    added random walk

--- markov.py
+++ markov.py
@@ -2,7 +2,7 @@ from dictogram import Dictogram
 import random
 import Stochastic
 from Modules import *
-
+import re
 def dicto(words):
     dict = {}
     for index in range(len(words) -1):
@@ -12,7 +12,17 @@ def dicto(words):
             dict[words[index]].add_count(words[index+1])
     return dict
 
+def randWalk(amount, dict):
+    sentence = ""
+    ctr = 0
+    while(ctr < amount):
+        dictogram = random.choice(list(dict))
+        sentence += (" " + dictogram)
+        ctr += 1
+    return sentence
 if __name__ == "__main__":
     words = stripFile()
     dictograms = dicto(words)
-    print(dictograms)
+    #print(dictograms)
+    sentence = randWalk(15,dictograms)
+    print(sentence)
